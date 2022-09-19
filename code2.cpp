f99f5292f5787c3d5a3b4c430837095b4f280230
Vaishali Aggarwal <0707vaishali@gmail.com>
2022-09-19 18:29:26 +0530

    n natural numbers sum

---
+++ code2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+    cout<<"the sum of n natural numbers is:"<<(n*(n+1))/2;
+    return 0;
+}

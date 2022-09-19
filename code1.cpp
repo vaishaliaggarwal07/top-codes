324dfdb8285105d36ffa613d736e63a7e2a56f60
Vaishali Aggarwal <0707vaishali@gmail.com>
2022-09-19 18:24:15 +0530

    odd even

---
+++ code1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter the number to check: ";
+    cin>>n;
+    if(n%2==0){
+        cout<<"number is even";
+    }
+    else{
+        cout<<"number is odd";
+    }
+    return 0;
+}
